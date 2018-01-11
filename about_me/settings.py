d8faa1a7159edeb2c68538023da062dba5c8cdc5
KentonParton <kknoxparton@gmail.com>
2018-01-10 22:31:06 -0600

    updates

--- about_me/settings.py
+++ about_me/settings.py
@@ -30,6 +30,27 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+
+#DEVELOPEMENT
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+
+# PRODUCTION
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+]
+
+
+
+
 # Application definition=
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -114,22 +135,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = 'static'
-
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, "static"),
-]
-
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
 
 #Login settings
 
